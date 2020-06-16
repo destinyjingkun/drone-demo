9067dc7eb428bd3534f98ef9dbe6a53ff9e015d7
Evan <destinyjingkun@vip.qq.com>
2020-06-16 11:03:59 +0800

    add http serve

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
 import "fmt"
+import "net/http"
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello world"))
+}
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println("server start")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":20080", nil)
 }
